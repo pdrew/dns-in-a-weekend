607fdf0d878deafd0df32da3c03790d46c494433
pdrew <patrick.r.drew@gmail.com>
2024-11-04 20:23:40 +1100

    parse dns response, implement resolver

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
-from dataclasses import dataclass
-import dataclasses
+from dataclasses import dataclass, astuple
 import struct
+import socket
+import random
+from io import BytesIO
+from typing import List
+
+random.seed(1)
+
+TYPE_A = 1
+CLASS_IN = 1
 
 @dataclass
 class DNSHeader:
@@ -17,8 +25,26 @@ class DNSQuestion:
     type_: int 
     class_: int 
 
+@dataclass
+class DNSRecord:
+    name: bytes
+    type_: int
+    class_: int
+    ttl: int
+    data: bytes 
+
+@dataclass
+class DNSPacket:
+    header: DNSHeader
+    questions: List[DNSQuestion]
+    # don't worry about the exact meaning of these 3 record
+    # sections for now: we'll use them in Part 3
+    answers: List[DNSRecord]
+    authorities: List[DNSRecord]
+    additionals: List[DNSRecord]
+
 def header_to_bytes(header):
-    fields = dataclasses.astuple(header)
+    fields = astuple(header)
     # there are 6 `H`s because there are 6 fields
     return struct.pack("!HHHHHH", *fields)
 
@@ -31,12 +57,6 @@ def encode_dns_name(domain_name):
         encoded += bytes([len(part)]) + part
     return encoded + b"\x00"
 
-import random
-random.seed(1)
-
-TYPE_A = 1
-CLASS_IN = 1
-
 def build_query(domain_name, record_type):
     name = encode_dns_name(domain_name)
     id = random.randint(0, 65535)
@@ -45,22 +65,73 @@ def build_query(domain_name, record_type):
     question = DNSQuestion(name=name, type_=record_type, class_=CLASS_IN)
     return header_to_bytes(header) + question_to_bytes(question)
 
-import socket
+def parse_header(reader):
+    items = struct.unpack("!HHHHHH", reader.read(12))
+    # see "a note on BytesIO" for an explanation of `reader` here
+    return DNSHeader(*items)
 
-query = build_query("www.example.com", 1)
+def decode_name_simple(reader):
+    parts = []
+    while (length := reader.read(1)[0]) != 0:
+        parts.append(reader.read(length))
+    return b".".join(parts)
 
-# create a UDP socket
-# `socket.AF_INET` means that we're connecting to the internet
-#                  (as opposed to a Unix domain socket `AF_UNIX` for example)
-# `socket.SOCK_DGRAM` means "UDP"
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def parse_question(reader):
+    name = decode_name_simple(reader)
+    data = reader.read(4)
+    type_, class_ = struct.unpack("!HH", data)
+    return DNSQuestion(name, type_, class_)
 
-# send our query to 8.8.8.8, port 53. Port 53 is the DNS port.
-sock.sendto(query, ("8.8.8.8", 53))
+def decode_name(reader):
+    parts = []
+    while (length := reader.read(1)[0]) != 0:
+        if length & 0b1100_0000:
+            parts.append(decode_compressed_name(length, reader))
+            break
+        else:
+            parts.append(reader.read(length))
+    return b".".join(parts)
 
-# read the response. UDP DNS responses are usually less than 512 bytes
-# (see https://www.netmeister.org/blog/dns-size.html for MUCH more on that)
-# so reading 1024 bytes is enough
-response, _ = sock.recvfrom(1024)
+def decode_compressed_name(length, reader):
+    pointer_bytes = bytes([length & 0b0011_1111]) + reader.read(1)
+    pointer = struct.unpack("!H", pointer_bytes)[0]
+    current_pos = reader.tell()
+    reader.seek(pointer)
+    result = decode_name(reader)
+    reader.seek(current_pos)
+    return result
 
-print(response)
+def parse_record(reader):
+    name = decode_name(reader)
+    # the the type, class, TTL, and data length together are 10 bytes (2 + 2 + 4 + 2 = 10)
+    # so we read 10 bytes
+    data = reader.read(10)
+    # HHIH means 2-byte int, 2-byte-int, 4-byte int, 2-byte int
+    type_, class_, ttl, data_len = struct.unpack("!HHIH", data) 
+    data = reader.read(data_len)
+    return DNSRecord(name, type_, class_, ttl, data)
+
+def parse_dns_packet(data):
+    reader = BytesIO(data)
+    header = parse_header(reader)
+    questions = [parse_question(reader) for _ in range(header.num_questions)]
+    answers = [parse_record(reader) for _ in range(header.num_answers)]
+    authorities = [parse_record(reader) for _ in range(header.num_authorities)]
+    additionals = [parse_record(reader) for _ in range(header.num_additionals)]
+
+    return DNSPacket(header, questions, answers, authorities, additionals)
+
+def ip_to_string(ip):
+    return ".".join([str(x) for x in ip])
+
+def lookup_domain(domain_name):
+    query = build_query(domain_name, TYPE_A)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(query, ("8.8.8.8", 53))
+
+    # get the response
+    data, _ = sock.recvfrom(1024)
+    response = parse_dns_packet(data)
+    return ip_to_string(response.answers[0].data)
+
+#ip = lookup_domain("www.example.com")
